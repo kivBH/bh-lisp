55f402cdf1297832feb915d9f4508e899aadfb6b
Patrik Harag <patrikharag@gmail.com>
2018-10-04 22:33:14 +0200

    Initial commit

---
+++ src/main/groovy/cz/bh/lisp/Main.groovy
@@ -0,0 +1,14 @@
+package cz.bh.lisp
+
+/**
+ *
+ * @version 2018-10-04
+ * @author Patrik Harag
+ */
+class Main {
+
+    static void main(String... args) {
+        println "BH Lisp REPL"
+    }
+
+}
---
+++ src/test/groovy/cz/bh/lisp/ExampleTest.groovy
@@ -0,0 +1,18 @@
+package cz.bh.lisp
+
+import org.junit.Test
+
+/**
+ *
+ * @version 2018-10-04
+ * @author Patrik Harag
+ */
+class ExampleTest {
+
+    @Test
+    void test() {
+        Main.main()
+        assert 2 == 1 + 1
+    }
+
+}
