e1fe91c3303846e4333dbd2a437beaea2c4e932f
Patrik Harag <patrikharag@gmail.com>
2018-10-06 21:21:25 +0200

    Add functions: mod, inc, dec

---
+++ src/main/groovy/cz/bh/lisp/lib/math/Dec.groovy
@@ -0,0 +1,29 @@
+package cz.bh.lisp.lib.math
+
+import cz.bh.lisp.lib.NativeFunctionDefinition
+import cz.bh.lisp.lib.Preconditions
+
+/**
+ * Defines the {@code dec} function.
+ *
+ * @version 2018-10-06
+ * @author Patrik Harag
+ */
+class Dec extends NativeFunctionDefinition {
+
+    @Override
+    String getSymbol() {
+        return "dec"
+    }
+
+    @Override
+    String getDoc() {
+        return "[x] Returns a number one less than x."
+    }
+
+    @Override
+    def run(List parameters) {
+        Preconditions.requireParameters(parameters, 1)
+        return Preconditions.requireType(parameters.first(), Number) - 1
+    }
+}
---
+++ src/main/groovy/cz/bh/lisp/lib/math/Mod.groovy
@@ -0,0 +1,31 @@
+package cz.bh.lisp.lib.math
+
+import cz.bh.lisp.lib.NativeFunctionDefinition
+import cz.bh.lisp.lib.Preconditions
+
+/**
+ * Defines the {@code mod} function.
+ *
+ * @version 2018-10-06
+ * @author Patrik Harag
+ */
+class Mod extends NativeFunctionDefinition {
+
+    @Override
+    String getSymbol() {
+        return "mod"
+    }
+
+    @Override
+    String getDoc() {
+        return "[n d] Returns the modulus of dividing numerator n by denominator d."
+    }
+
+    @Override
+    def run(List parameters) {
+        Preconditions.requireParameters(parameters, 2)
+        def n = Preconditions.requireType(parameters[0], Number)
+        def d = Preconditions.requireType(parameters[1], Number)
+        return n % d
+    }
+}
